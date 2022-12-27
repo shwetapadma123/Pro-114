2b6f8762d7140724074673c819ed5ddff2dfac7b
shwetapadma123 <120262649+shwetapadma123@users.noreply.github.com>
2022-12-27 19:11:25 +0530

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,11 @@
+a = 33
+b = '33'
+if b > a:
+    print('b is greater than a')
+elif a == b:
+    print("a and b are equal")
+    def my_function(fname):
+       print(fname + "Refsnes")
+my_function('Email')
+my_function('Tobias')
+my_function('Linus')
